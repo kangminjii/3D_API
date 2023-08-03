c1b3bb7aa0874c69d58f1c137c897f0aea839d38
rkdalswl789 <rkdalswl789@gmail.com>
2023-08-03 10:46:46 +0900

    0803 최종

--- 3D_API/cMainGame.h
+++ 3D_API/cMainGame.h
@@ -23,14 +23,36 @@ private:
 	cVector3	m_vLookAt;
 	cVector3	m_vUp;
 
-	cVector3	m_vPosition; // 위치
-	float		m_fBoxRotY;   // 회전
+	cVector3	m_vPosition;	 // 위치
+	float		m_fBoxRotY;		 // 회전
+	cVector3	m_vBoxDirection; // 방향 벡터
+
+	// >> : grid
+	std::vector<cVector3>	m_vecLineVertex;
+	cVector3				m_vAxisXTextPosition;
+	cVector3				m_vAxisZTextPosition;
+	// << :
+
+	POINT		m_ptPrevMouse;
+	bool		m_isLButtonDown;
+	float		m_fCameraDistance; // 카메라 거리 조정
+	cVector3	m_vCamRotAngle;
+
+	float m_fScale; // 크기
+
 
 public:
 	void Setup();  // 초기값 정리
 	void Update();
 	void Render(HDC hdc);
 	void WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	bool IsBackFace(cVector3& v1, cVector3& v2, cVector3& v3);
 
+	void SetGrid();
+	void DrawGrid();
+
+	void Update_Rotation();
+	void Update_Move();
+	void Update_Scale();
 };
 
